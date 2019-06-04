0c05790158b6ae9ad20fca77d7d23ef671bc62d0
krystal montesdeoca <krystal.montesdeoca@gmail.com>
2019-06-04 18:05:05 +0200

    added the Add A New Task option on the index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # READ & CREATE
   get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: :new
   post 'tasks', to: 'tasks#create'
   # UPDATE
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
