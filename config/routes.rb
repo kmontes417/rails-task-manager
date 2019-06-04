68317495ae23d80dd5c1cef1f3756908c9456473
krystal montesdeoca <krystal.montesdeoca@gmail.com>
2019-06-04 17:42:59 +0200

    finished but still need to refactor the new/edit and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # READ & CREATE
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
